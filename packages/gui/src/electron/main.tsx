77b6fcaabc0a503d677f7c5f08eb641ae55f69a8
LloydMontgomeryGarmadon <crazymathew29@gmail.com>
2023-09-05 20:43:05 +0200

    CRYPTOMINES

--- packages/gui/src/electron/main.tsx
+++ packages/gui/src/electron/main.tsx
@@ -71,7 +71,7 @@ let mainWindow: BrowserWindow | null = null;
 let currentDownloadRequest: any;
 let abortDownloadingFiles: boolean = false;
 
-// Set the userData directory to its location within CHIA_ROOT/gui
+// Set the userData directory to its location within CRYPTOMINES_ROOT/gui
 setUserDataDir();
 
 function renderAbout(): string {
